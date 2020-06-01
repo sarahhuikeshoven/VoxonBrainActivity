504266ba4ee1e121c16be3148bf51437ef45065a
SarahHuik <sarah.huikeshoven@accenture.com>
2020-06-01 14:02:09 +0200

    Update
    server updated to work with Heroku

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 // server.js
- 
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
+var express = require('express')
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+
+
 const WebSocket = require('ws')
  
-const wss = new WebSocket.Server({ port: 4321 })
+const wss = new WebSocket.Server({ server })
 let action
 let clients
 let jsonObject
